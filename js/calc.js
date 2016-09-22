5a648e173ba7ffb987e061550003bb887b8a1279
Cam Hering and Marty Kovach <marty@>
2016-09-21 18:47:40 -0700

    seperate the bussiness logic from the front end logic, and add branching front end logic for adding,subtracting,multiplying, and dividing.

--- js/calc.js
+++ js/calc.js
@@ -1,68 +1,71 @@
-//var add= function(number1, number2){
-//return number1 + number2;
-//};
+// Business logic
+var add= function(number1, number2){
+return number1 + number2;
+};
 
-//var number1 = parseInt(prompt("Enter a number:"));
+var subtract= function(number1, number2){
+  return number1 - number2;
+};
 
-//var number2 = parseInt(prompt("Enter another number:"));
-
-//alert(add(number1, number2));
-
-//var subtract= function(number1, number2){
-//  return number1 - number2;
-//};
-
-//var number1 = parseInt(prompt("Enter a number:"));
-
-//var number2 = parseInt(prompt("Enter another number:"));
-
-//alert(subtract(number1, number2));
 
 var multiply= function(number1, number2){
   return number1 * number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-
-var number2 = parseInt(prompt("Enter another number:"));
-
-alert(multiply(number1, number2));
 
 var divide= function(number1, number2){
   return number1 / number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
 
-var number2 = parseInt(prompt("Enter another number:"));
 
-alert(divide(number1, number2));
+//var bmi = function(weight, height) {
+//return (weight * 703)/ (height * height);
+//};
+//var weight = parseInt(prompt("Enter your weight in pounds:"));
 
-var bmi = function(weight, height) {
-return (weight * 703)/ (height * height);
-};
-var weight = parseInt(prompt("Enter your weight in pounds:"));
-
-var height = parseInt(prompt("Enter your height in inches:"));
+//var height = parseInt(prompt("Enter your height in inches:"));
 
 
 
-alert(bmi(weight, height));
+//alert(bmi(weight, height));
 
 
-var fToc = function(fare, cels) {
-return (fare - 32) * 5/9;
-};
+//var fToc = function(fare, cels) {
+//return (fare - 32) * 5/9;
+//};
 
-var fare = parseInt(prompt("Enter the temperature in Farenheit:"));
+//var fare = parseInt(prompt("Enter the temperature in Farenheit:"));
 
-alert(fToc(fare));
+//alert(fToc(fare));
 
 
-var cToc = function(celc) {
-return (celc * 9/5) + 32;
-};
+//var cToc = function(celc) {
+//return (celc * 9/5) + 32;
+//};
 
-var celc = parseInt(prompt("Enter the temperature in Celsuis:"));
+//var celc = parseInt(prompt("Enter the temperature in Celsuis:"));
 
-alert(cToc(celc));
+//alert(cToc(celc));
+
+//Front End logic
+
+$(document).ready(function() {
+  $("form#calculator").submit(function() {
+    event.preventDefault();
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+    $("#output").text(result);
+  });
+});
